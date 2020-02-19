0b4c1fce956e1a833958c110493e75ca84d93234
The1984 <aranibar.javier@gmail.com>
2020-02-18 22:47:39 -0300

    control de error, incorporar un loadery simulacion de consumir API

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Loading.css'
+
+const Loading = () => 
+(
+    // https://projects.lukehaas.me/css-loaders/
+    <div class="loader">
+        Loading...
+    </div>
+)
+
+export default Loading
